acc86b5b9b719845cb934945570ef2d9b2ce8c4b
jchanke <josephc2@andrew.cmu.edu>
2024-07-08 09:49:10 -0400

    [Edit] Completed Challenge 7

--- index.js
+++ index.js
@@ -165,7 +165,14 @@ const sortedGames = GAMES_JSON.sort((item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [game1, game2, ...rest] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const topPledgeGame = document.createElement("p");
+topPledgeGame.innerHTML = game1.name;
+firstGameContainer.appendChild(topPledgeGame);
 
 // do the same for the runner up item
+const runnerUpPledgeGame = document.createElement("p");
+runnerUpPledgeGame.innerHTML = game2.name;
+secondGameContainer.appendChild(runnerUpPledgeGame);
